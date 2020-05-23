22ed52e5fe296479db3e9486349c0014f87999ab
Gareth Ferguson <gareth.f@hotmail.co.uk>
2020-05-23 16:48:45 +0100

    turn nav dots into a stand alone component

--- src/components/Home/panel.js
+++ src/components/Home/panel.js
@@ -1,14 +1,43 @@
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Container from "../container"
 import Heading from "../Typography/heading"
 import { Link } from "gatsby"
 import React from "react"
+import TransitionLink from "gatsby-plugin-transition-link"
 import { gatsbyWindow } from "../../helpers/gatsbyWindow"
+import gsap from "gsap"
 import styled from "@emotion/styled"
+export const fade = ({ exit: { length }, node, direction }) => {
+    const duration = direction === "out" ? length + length / 4 : length
+    const opacity = direction === "in" ? 1 : 0
+    const scrollTop =
+        (document.scrollingElement && document.scrollingElement.scrollTop) ||
+        document.body.scrollTop ||
+        window.pageYOffset
+    const holdPosition =
+        direction === "out"
+            ? {
+                  height: "100%",
+                  overflow: "scroll",
+                  position: "absolute",
+                  scrollTop: window.scrollY,
+              }
+            : {}
+
+    return gsap
+        .timeline()
+        .set(node, holdPosition)
+        .fromTo(node, { opacity: !opacity }, { opacity: opacity, duration })
+}
 
 const PanelText = styled(Container)`
     position: relative;
     padding-bottom: 2rem;
     z-index: 2;
+    display: table-cell;
+    vertical-align: bottom;
+    width: 100%;
+    height: 100%;
 `
 
 const PanelTopHeading = styled(Heading)`
@@ -32,6 +61,16 @@ const PanelSubText = styled.p`
     }
 `
 
+export const FadeLink = ({ to, children }) => (
+    <TransitionLink
+        enter={{ delay: 0, length: 0 }}
+        exit={{ delay: 0.5, length: 0.5 }}
+        to={to}
+    >
+        {children}
+    </TransitionLink>
+)
+
 const Panel = ({
     children,
     image,
@@ -48,6 +87,7 @@ const Panel = ({
             ? image.resolutions.srcWebp
             : image.file.url
     }
+
     return (
         <>
             {children}
@@ -55,13 +95,49 @@ const Panel = ({
                 <PanelText>
                     {topText && (
                         <PanelTopHeading level="h1">
-                            {slug && <Link to={`/${slug}`}>{topText}</Link>}
+                            {slug && (
+                                <AniLink swipe to={`/${slug}`}>
+                                    {topText}
+                                </AniLink>
+                            )}
                             {!slug && topText}
                         </PanelTopHeading>
                     )}
                     {text && (
                         <PanelHeading level="h2" lower={topText}>
-                            {slug && <Link to={`/${slug}`}>{text}</Link>}
+                            {slug && (
+                                <AniLink
+                                    trigger={async pages => {
+                                        const s = window.scrollY
+                                        window.scrollTo(0, 0)
+
+                                        const div = document.getElementById(
+                                            "home-container"
+                                        )
+                                        div.style.position = "relative"
+                                        div.style.top = `-${s}px`
+                                        pages.entry.state = { test: "sdf" }
+                                        const exit = await pages.exit
+                                        const entry = await pages.entry
+                                        entry.node.querySelector(
+                                            "video"
+                                        ).currentTime = exit.node.querySelector(
+                                            "video"
+                                        ).currentTime
+                                    }}
+                                    entry={{
+                                        state: {
+                                            transition:
+                                                "passed to the entering page",
+                                        },
+                                    }}
+                                    fade
+                                    to={`/${slug}`}
+                                    duration={1}
+                                >
+                                    {text}
+                                </AniLink>
+                            )}
                             {!slug && text}
                         </PanelHeading>
                     )}
