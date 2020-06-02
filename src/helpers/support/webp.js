5c40befe27e9a155e21d2ff5c43099ae1e8fa714
Gareth Ferguson <gareth.f@hotmail.co.uk>
2020-06-03 00:40:47 +0100

    fix webp

--- src/helpers/support/webp.js
+++ src/helpers/support/webp.js
@@ -2,8 +2,13 @@ let support
 export const supportsWebP = () => {
     if (typeof support !== "undefined") return support
 
+    if (typeof document === `undefined`) {
+        // No browser
+        return true
+    }
+
     const elem =
-        typeof document === "object" ? document.createElement("canvas") : {}
+        typeof document === "object" ? document.createElement("canvas") : false
 
     support = elem.toDataURL("image/webp").indexOf("data:image/webp") === 0
 
